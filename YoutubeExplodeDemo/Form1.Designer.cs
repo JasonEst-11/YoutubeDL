52623ce47f48ce5e771ecfdd436826ba60acb868
Jesteban-11 <estebanfrancisjaeson@outlook.com>
2023-05-13 16:58:41 +0300

    Add project files.

---
+++ YoutubeExplodeDemo/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace YoutubeExplodeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.textboxURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnFileExplorer = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.textboxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetData.Location = new System.Drawing.Point(561, 12);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(43, 33);
+            this.btnGetData.TabIndex = 0;
+            this.btnGetData.Text = "🔍";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textboxURL
+            // 
+            this.textboxURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxURL.Location = new System.Drawing.Point(74, 12);
+            this.textboxURL.Name = "textboxURL";
+            this.textboxURL.Size = new System.Drawing.Size(481, 30);
+            this.textboxURL.TabIndex = 1;
+            this.textboxURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textboxURL_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Duration:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Author:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(128, 100);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(26, 25);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "--";
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDuration.Location = new System.Drawing.Point(128, 150);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(26, 25);
+            this.lblDuration.TabIndex = 6;
+            this.lblDuration.Text = "--";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAuthor.Location = new System.Drawing.Point(128, 125);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(26, 25);
+            this.lblAuthor.TabIndex = 7;
+            this.lblAuthor.Text = "--";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Download path:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Download 🎵";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(200, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 46);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Download 🎥";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnFileExplorer
+            // 
+            this.btnFileExplorer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFileExplorer.Location = new System.Drawing.Point(561, 46);
+            this.btnFileExplorer.Name = "btnFileExplorer";
+            this.btnFileExplorer.Size = new System.Drawing.Size(43, 34);
+            this.btnFileExplorer.TabIndex = 12;
+            this.btnFileExplorer.Text = "📂";
+            this.btnFileExplorer.UseVisualStyleBackColor = true;
+            this.btnFileExplorer.Click += new System.EventHandler(this.btnFileExplorer_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "URL:";
+            // 
+            // logBox
+            // 
+            this.logBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.logBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logBox.ForeColor = System.Drawing.Color.White;
+            this.logBox.Location = new System.Drawing.Point(10, 178);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(594, 155);
+            this.logBox.TabIndex = 14;
+            // 
+            // textboxPath
+            // 
+            this.textboxPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxPath.Location = new System.Drawing.Point(166, 48);
+            this.textboxPath.Name = "textboxPath";
+            this.textboxPath.Size = new System.Drawing.Size(389, 30);
+            this.textboxPath.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 397);
+            this.Controls.Add(this.textboxPath);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnFileExplorer);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblDuration);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxURL);
+            this.Controls.Add(this.btnGetData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YoutubeDL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.TextBox textboxURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnFileExplorer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.TextBox textboxPath;
+    }
+}
+
---
+++ YoutubeExplodeDemo/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeExplodeDemo
+{
+    public partial class Form1 : Form
+    {
+        public YoutubeClient youtube = new YoutubeClient();
+        public string DlPath = "C:\\Users\\esteb\\Music"; //Default
+        public FolderBrowserDialog BrowserDialog;
+
+        public ToolTip ttip = new ToolTip();
+
+        public Video LoadedVideo;
+        public Form1()
+        {
+            InitializeComponent();
+            textboxPath.Text = DlPath;
+            SetTooltips();
+        }
+
+        private void SetTooltips()
+        {            
+            ttip.SetToolTip(lblTitle, lblTitle.Text);
+            ttip.SetToolTip(lblAuthor, lblAuthor.Text);
+            ttip.SetToolTip(lblDuration, lblDuration.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetData();            
+        }
+
+        private async void GetData() //Get Data
+        {
+            try
+            {
+                LogMessage("Fetching Data from URL...");
+                LoadedVideo = await youtube.Videos.GetAsync(textboxURL.Text);
+                LogMessage("Data fetched successfully!");
+                lblTitle.Text = LoadedVideo.Title;
+                lblAuthor.Text = LoadedVideo.Author.ChannelTitle;
+                lblDuration.Text = LoadedVideo.Duration.ToString();
+                SetTooltips();
+            }
+            catch (Exception ex)
+            {
+                LogMessage(ex.Message);
+            }
+
+        }
+        private void btnFileExplorer_Click(object sender, EventArgs e) //Folder Browser Dialog
+        {
+            BrowserDialog = new FolderBrowserDialog();
+            DialogResult result = BrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {                
+                textboxPath.Text = BrowserDialog.SelectedPath;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e) //Get Audio
+        {
+            if (LoadedVideo != null)
+            {
+                try
+                {
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(textboxURL.Text);
+                    var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+                    LogMessage($"Downloading Audio: {lblTitle.Text}...");
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{textboxPath.Text}\\{lblTitle.Text}.mp3");
+                    LogMessage("Download Complete!");
+                }
+                catch (Exception ex)
+                {
+                    LogMessage(ex.Message);
+                }
+            }
+            else
+            {
+                LogMessage("No video loaded");
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e) //Get Video
+        {
+            if (LoadedVideo != null)
+            {
+                try
+                {
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(textboxURL.Text);
+                    var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+                    LogMessage($"Downloading Video: {lblTitle.Text}...");
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{textboxPath.Text}\\{lblTitle.Text}.{streamInfo.Container}");
+                    LogMessage("Download Complete!");
+                }
+                catch (Exception ex)
+                {
+                    LogMessage(ex.Message);
+                }
+            }
+            else
+            {
+                LogMessage("No video loaded.");
+            }
+
+        }
+
+        private void LogMessage(string message)
+        {
+            logBox.Text += $"{DateTime.Now.ToString()}: {message} {Environment.NewLine}";
+        }
+
+        private void textboxURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                GetData();
+            }
+        }
+    }
+}
